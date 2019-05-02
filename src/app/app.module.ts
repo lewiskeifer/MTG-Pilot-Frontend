4d36e47fbb2651147dcb6de610d12a44a8b46088
unknown <lewiskeifer@yahoo.com>
2019-05-01 20:11:40 -0400

    added materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 import { DeckSearchComponent } from './deck-search/deck-search.component';
 import { DeckListComponent } from './deck-list/deck-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCard, MatCardModule } from '@angular/material';
  
 @NgModule({
   declarations: [
@@ -27,14 +29,16 @@ import { DeckListComponent } from './deck-list/deck-list.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
     HttpClientModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    // HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }
+    // )
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
