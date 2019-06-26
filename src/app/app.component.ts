c3e15f9ac8bb1a104830f540a46d99487d51bb8b
Keifer Lewis <lewiskeifer@yahoo.com>
2019-06-26 12:40:32 -0400

    welcome page bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   
-  title = 'MTG Finance Manager';
+  title = 'MTG Pilot';
   currentUser: User;
   
   constructor(private router: Router, private authenticationService: AuthenticationService) {
